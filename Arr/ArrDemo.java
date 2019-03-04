17aa0c98ee0497e9dd7eb1afd6e509064eb319a0
zm-zn <41509040226zm>
2019-03-04 19:20:19 +0800

    第一次提交代码

---
+++ Arr/ArrDemo.java
@@ -0,0 +1,12 @@
+public class ArrDemo{
+	public static void main(String[] args){
+		int[] x = new int[3];
+		System.out.println(x.length);
+		x[0] = 1;
+		x[1] = 2;
+		x[2] = 3;
+	  for (int i=0;i<=x.length;i++){
+		System.out.println(xs[i]);
+	   }
+	}
+}
